f20fae0b03f5c35c49e11be984268303aa28110e
Doctor Blue <Doctorblue.king@gmail.com>
2019-11-05 17:19:06 +0700

    add TodayFragment. date 17:20 5/11/2019

---
+++ app/src/main/java/com/doctor/blue/pedometer_countyoursteps/fragments/MonthAndWeekFragment.java
@@ -0,0 +1,17 @@
+package com.doctor.blue.pedometer_countyoursteps.fragments;
+
+import com.doctor.blue.pedometer_countyoursteps.R;
+import com.doctor.blue.pedometer_countyoursteps.base.BaseFragment;
+
+public class MonthAndWeekFragment extends BaseFragment {
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.fragment_month_and_week;
+    }
+
+    @Override
+    public void init() {
+
+    }
+}
